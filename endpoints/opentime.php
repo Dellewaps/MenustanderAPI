9be6b24f82cb6d110fccde9dd666547fc8c1430c
Delle <dellewaps@gmail.com>
2019-08-23 09:01:29 +0200

    opentimes added

---
+++ endpoints/opentime.php
@@ -0,0 +1,31 @@
+<?php
+include_once ("../database.inc");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+//Laver en connection til databasen og sætter den til en lokal variabel
+$database = new Database();
+$db = $database->getConnection();
+
+$query = "SELECT * FROM opentimes";
+$stmt = $db->prepare($query);
+    try{
+        $stmt->execute();
+        $open_arr=array();
+        $open_arr["records"]=array();
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $open_item=array(
+                "Id" => $Id,
+                "open" => $open,
+                "closed" => $closed,
+            );
+            // Tager dataet og pusher til det array som blev lavet til at store selve datasættet
+            array_push($open_arr["records"], $open_item);
+        }
+        http_response_code(200);
+        $JSON = json_encode($open_arr);
+        print_r($JSON);
+    } catch(PDOException $e){
+        return($e);
+    }
--- endpoints/opentimespost.php
+++ endpoints/opentimespost.php
@@ -1,36 +1,77 @@
 <?php
-include_once ("../database.inc");
+include_once("../database.inc");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 //Ser efter om den requst der bliver sendt er en POST request
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //Laver en connection til databasen og sætter den til en lokal variabel
     $database = new Database();
     $db = $database->getConnection();
 
+    if (isset($_POST['morningOpen']) && isset($_POST['morningClose'])) {
+        //Sætter variabler til at være de informationer der kommer fra $post requster
+        $Id = 1;
+        $open = $_POST['morningOpen'];
+        $closed = $_POST['morningClose'];
 
-    //Sætter variabler til at være de informationer der kommer fra $post requster
-    $open = $_POST['open'];
-    $closed = $_POST['closed'];
+        //Laver en query statment med det som skal sættes ind i databasen
+        $query = " UPDATE opentimes SET open = '$open', closed = '$closed' WHERE Id = $Id ";
+        //Forbereder query statment for at checke at den er en valid query
+        $stmt = $db->prepare($query);
+        // execute query 
+        $stmt->execute();
+    }
+    if (isset($_POST['beforeDinnerOpen']) && isset($_POST['beforeDinnerClose'])) {
+        //Sætter variabler til at være de informationer der kommer fra $post requster
+        $Id = 2;
+        $open = $_POST['beforeDinnerOpen'];
+        $closed = $_POST['beforeDinnerClose'];
 
+        //Laver en query statment med det som skal sættes ind i databasen
+        $query = " UPDATE opentimes SET open = '$open', closed = '$closed' WHERE Id = $Id ";
+        //Forbereder query statment for at checke at den er en valid query
+        $stmt = $db->prepare($query);
+        // execute query 
+        $stmt->execute();
+        } 
+    if (isset($_POST['dinnerOpen']) && isset($_POST['dinnerClose'])) {
+        //Sætter variabler til at være de informationer der kommer fra $post requster
+        $Id = 3;
+        $open = $_POST['dinnerOpen'];
+        $closed = $_POST['dinnerClose'];
 
-    //Laver en query statment med det som skal sættes ind i databasen
-    $query = " UPDATE opentimes (open, closed) SET ('$open', '$closed') ";
-    
+        //Laver en query statment med det som skal sættes ind i databasen
+        $query = " UPDATE opentimes SET open = '$open', closed = '$closed' WHERE Id = $Id ";
+        //Forbereder query statment for at checke at den er en valid query
+        $stmt = $db->prepare($query);
+        // execute query 
+        $stmt->execute();
+    } 
+    if (isset($_POST['afternoonOpen']) && isset($_POST['afternoonClose'])) {
+        //Sætter variabler til at være de informationer der kommer fra $post requster
+        $Id = 4;
+        $open = $_POST['afternoonOpen'];
+        $closed = $_POST['afternoonClose'];
 
-    //Forbereder query statment for at checke at den er en valid query
-    $stmt = $db->prepare($query);
-        // Prøver at execute statement queriet for at se om der bliver meldt fejl af databasen
-        try {
-            // execute query og return den for at lave check på den
-            $stmt->execute();
-            $out['result'] = 1;
-            $out['message'] = "Retten er nu gemt";
-            $json = json_encode($out);
-            return print_r($json);
-        } catch (PDOException $e) {
-            // Returner error koden så der kan laves håndtering på den
-            return ($e);
-        }
+        //Laver en query statment med det som skal sættes ind i databasen
+        $query = " UPDATE opentimes SET open = '$open', closed = '$closed' WHERE Id = $Id ";
+        //Forbereder query statment for at checke at den er en valid query
+        $stmt = $db->prepare($query);
+        // execute query 
+        $stmt->execute();
+    }     
+    // Prøver at execute statement queriet for at se om der bliver meldt fejl af databasen
+    try {
+        //return den for at lave check på den
+        $out['result'] = 1;
+        $out['message'] = "Åbningstider er gemt";
+        $json = json_encode($out);
+        return print_r($json);
+    } catch (PDOException $e) {
+        // Returner error koden så der kan laves håndtering på den
+        return ($e);
+    }
+
 }
+
